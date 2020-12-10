5898a8172ce082420c08ab75a27b1c2ca8fd6984
Tami Tami <68597117+tamitami5c@users.noreply.github.com>
2020-12-10 15:52:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ def download_file(url):
     return file_name
 
 def daterange(start_date,end_date):
-    for n in range(int((end_date-start_date).days)):
+    """
+    start_date(含む)からend_date(含む)まで
+    """
+    for n in range(int((end_date-start_date).days)+1):
         yield start_date+datetime.timedelta(n)
 
 def main():
